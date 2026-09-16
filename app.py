50b02e9c44e51e8a23bec43c58e0978f1e8ba6b1
Mahek2725 <mahekkarwat353@gmail.com>
2026-09-17 01:17:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def ask_gemini(prompt):
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=prompt
         )
 
